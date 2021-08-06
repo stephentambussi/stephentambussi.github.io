679a916b91cb0bb5af3760172c528f9504ee50c1
Stephen Tambussi <31783116+stephentambussi@users.noreply.github.com>
2021-08-06 15:34:37 -0700

    experience page should work now

--- frontend/experience/wd.js
+++ frontend/experience/wd.js
@@ -1,17 +1,12 @@
 
-var modal = document.getElementById("modal");
+var modal = document.getElementById("wd-modal");
 
-// Get image and insert it inside modal -- uses alt text as caption
+// Get text and insert it inside modal
 var img = document.getElementById("WD");
-var description = document.getElementById("WD_Text")
-var modalText = document.getElementById("text_src");
-var captionText = document.getElementById("caption");
+
 img.onclick = function()
 {
     modal.style.display = "block";
-    description.style.display = "block";
-    modalText.innerHTML = description;
-    captionText.innerHTML = this.alt;
 }
 
 // Get span element to close modal
@@ -21,12 +16,10 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() 
 {
     modal.style.display = "none";
-    description.style.display = "none";
 }
 
 document.addEventListener('keydown', (event) => {
     if (event.key == 'Escape') {
         modal.style.display = "none";
-        description.style.display = "none";
     }
 });
