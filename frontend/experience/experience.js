94bd5f9b5cbd61395a1d5d34ca3b1ebc12d26702
Stephen Tambussi <31783116+stephentambussi@users.noreply.github.com>
2021-08-17 16:05:16 -0700

    code rewrite again js

--- frontend/experience/experience.js
+++ frontend/experience/experience.js
@@ -1,7 +1,5 @@
 
-var wd_modal = document.getElementById("wd-modal");
-var scu_modal = document.getElementById("scu-modal");
-var marvell_modal = document.getElementById("marvell-modal");
+var modal = document.getElementsByClassName("modal");
 
 var skills = document.getElementsByClassName("skills");
 
@@ -14,21 +12,21 @@ var img3 = document.getElementsById("M");
 
 img1.onclick = function()
 {
-    wd_modal.style.display = "flex";
+    modal[0].style.display = "flex";
     skills[0].style.display = "inline-flex";
     body[0].style.overflowY = "hidden";
 }
 
 img2.onclick = function()
 {
-    scu_modal.style.display = "flex";
+    modal[1].style.display = "flex";
     skills[1].style.display = "inline-flex";
     body[1].style.overflowY = "hidden";
 }
 
 img3.onclick = function()
 {
-    marvell_modal.style.display = "flex";
+    modal[2].style.display = "flex";
     skills[2].style.display = "inline-flex";
     body[2].style.overflowY = "hidden";
 }
@@ -39,29 +37,25 @@ var span = document.getElementsByClassName("close");
 // When click on span (x), close the modal
 span[0].onclick = function() 
 {
-    wd_modal.style.display = "none";
+    modal[0].style.display = "none";
     body[0].style.overflowY = "auto";
 }
 
 span[1].onclick = function() 
 {
-    scu_modal.style.display = "none";
+    modal[1].style.display = "none";
     body[1].style.overflowY = "auto";
 }
 
 span[2].onclick = function() 
 {
-    marvell_modal.style.display = "none";
+    modal[2].style.display = "none";
     body[2].style.overflowY = "auto";
 }
 
 document.addEventListener('keydown', (event) => {
     if (event.key == 'Escape') {
-        wd_modal.style.display = "none";
-        scu_modal.style.display = "none";
-        marvell_modal.style.display = "none";
-        body[0].style.overflowY = "auto";
-        body[1].style.overflowY = "auto";
-        body[2].style.overflowY = "auto";
+        modal.style.display = "none";
+        body.style.overflowY = "auto";
     }
 });
