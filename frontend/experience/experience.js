ffd71368acd72ab42fa7bca380d3c2b5cefdf585
Stephen Tambussi <31783116+stephentambussi@users.noreply.github.com>
2021-08-18 18:58:15 -0700

    Trying to make X button work

--- frontend/experience/experience.js
+++ frontend/experience/experience.js
@@ -17,47 +17,28 @@ for (var i = 0; i < btns.length; i++) {
     thisBtn.addEventListener("click", function () {
         var modal = document.getElementById(this.dataset.modal);
         modal.style.display = "flex";
+        body.style.overflowY = "hidden";
     }, false);
 }
 
-btns[0].onclick = function()
-{
-    wd_modal.style.display = "flex";
-    skills[0].style.display = "inline-flex";
-    body[0].style.overflowY = "hidden";
-}
-
-img2.onclick = function()
-{
-    modals[1].style.display = "flex";
-    skills[1].style.display = "inline-flex";
-    body[1].style.overflowY = "hidden";
-}
-
-img3.onclick = function()
-{
-    modals[2].style.display = "flex";
-    skills[2].style.display = "inline-flex";
-    body[2].style.overflowY = "hidden";
-}
 
 // When click on span (x), close the modal
-span[0].onclick = function() 
+spans[0].onclick = function() 
 {
     modals[0].style.display = "none";
-    body[0].style.overflowY = "auto";
+    body.style.overflowY = "auto";
 }
 
-span[1].onclick = function() 
+spans[1].onclick = function() 
 {
     modals[1].style.display = "none";
-    body[1].style.overflowY = "auto";
+    body.style.overflowY = "auto";
 }
 
-span[2].onclick = function() 
+spans[2].onclick = function() 
 {
     modals[2].style.display = "none";
-    body[2].style.overflowY = "auto";
+    body.style.overflowY = "auto";
 }
 
 document.addEventListener('keydown', (event) => {
