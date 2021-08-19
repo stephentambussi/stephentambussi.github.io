9e0a0f96cea01d5385f74efd06bd86d191517f51
Stephen Tambussi <31783116+stephentambussi@users.noreply.github.com>
2021-08-18 19:02:33 -0700

    formatting hopefully

--- frontend/experience/experience.js
+++ frontend/experience/experience.js
@@ -1,13 +1,4 @@
-
-var modals = document.getElementsByClassName("modal");
-var wd_modal = document.getElementById("wd-modal");
-
-var skills = document.getElementsByClassName("skills");
-
 var body = document.getElementsByClassName("body");
-
-// Get span element to close modal
-var spans = document.getElementsByClassName("close");
 
 // Get image object and use it to expand text
 var btns = document.getElementsByClassName("open-modal");
@@ -17,33 +8,30 @@ for (var i = 0; i < btns.length; i++) {
     thisBtn.addEventListener("click", function () {
         var modal = document.getElementById(this.dataset.modal);
         modal.style.display = "flex";
-        body.style.overflowY = "hidden";
+        modal.style.overflowY = "hidden";
     }, false);
 }
 
+var modals = document.getElementsByClassName("modal");
+
+// Get span element to close modal
+var spans = document.getElementsByClassName("close");
 
 // When click on span (x), close the modal
 spans[0].onclick = function() 
 {
     modals[0].style.display = "none";
-    body.style.overflowY = "auto";
+    //body.style.overflowY = "auto";
 }
 
 spans[1].onclick = function() 
 {
     modals[1].style.display = "none";
-    body.style.overflowY = "auto";
+    //body.style.overflowY = "auto";
 }
 
 spans[2].onclick = function() 
 {
     modals[2].style.display = "none";
-    body.style.overflowY = "auto";
+    //body.style.overflowY = "auto";
 }
-
-document.addEventListener('keydown', (event) => {
-    if (event.key == 'Escape') {
-        modals.style.display = "none";
-        body.style.overflowY = "auto";
-    }
-});
