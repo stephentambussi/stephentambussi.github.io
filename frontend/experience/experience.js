7fc421ce2c029cef250ab2497c99eb6c3cd660b7
Stephen Tambussi <31783116+stephentambussi@users.noreply.github.com>
2021-08-18 18:54:54 -0700

    trying to fix modal experience page

--- frontend/experience/experience.js
+++ frontend/experience/experience.js
@@ -1,18 +1,26 @@
 
-var modal = document.getElementsByClassName("modal");
+var modals = document.getElementsByClassName("modal");
 var wd_modal = document.getElementById("wd-modal");
 
 var skills = document.getElementsByClassName("skills");
 
 var body = document.getElementsByClassName("body");
 
-// Get image object and use it to expand text
-// var img1 = document.getElementById("WD");
-var btn = document.getElementsByClassName("open-modal");
-var img2 = document.getElementsById("SCU");
-var img3 = document.getElementsById("M");
+// Get span element to close modal
+var spans = document.getElementsByClassName("close");
 
-btn[0].onclick = function()
+// Get image object and use it to expand text
+var btns = document.getElementsByClassName("open-modal");
+
+for (var i = 0; i < btns.length; i++) {
+    var thisBtn = btns[i];
+    thisBtn.addEventListener("click", function () {
+        var modal = document.getElementById(this.dataset.modal);
+        modal.style.display = "flex";
+    }, false);
+}
+
+btns[0].onclick = function()
 {
     wd_modal.style.display = "flex";
     skills[0].style.display = "inline-flex";
@@ -21,43 +29,40 @@ btn[0].onclick = function()
 
 img2.onclick = function()
 {
-    modal[1].style.display = "flex";
+    modals[1].style.display = "flex";
     skills[1].style.display = "inline-flex";
     body[1].style.overflowY = "hidden";
 }
 
 img3.onclick = function()
 {
-    modal[2].style.display = "flex";
+    modals[2].style.display = "flex";
     skills[2].style.display = "inline-flex";
     body[2].style.overflowY = "hidden";
 }
 
-// Get span element to close modal
-var span = document.getElementsByClassName("close");
-
 // When click on span (x), close the modal
 span[0].onclick = function() 
 {
-    modal[0].style.display = "none";
+    modals[0].style.display = "none";
     body[0].style.overflowY = "auto";
 }
 
 span[1].onclick = function() 
 {
-    modal[1].style.display = "none";
+    modals[1].style.display = "none";
     body[1].style.overflowY = "auto";
 }
 
 span[2].onclick = function() 
 {
-    modal[2].style.display = "none";
+    modals[2].style.display = "none";
     body[2].style.overflowY = "auto";
 }
 
 document.addEventListener('keydown', (event) => {
     if (event.key == 'Escape') {
-        modal.style.display = "none";
+        modals.style.display = "none";
         body.style.overflowY = "auto";
     }
 });
